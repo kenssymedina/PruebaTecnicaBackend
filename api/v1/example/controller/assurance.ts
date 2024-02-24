81050e1f97946b526d55ba2c5ee845669da8b836
Kenssy Medina <kenssymedina2014@gmail.com>
2024-02-23 19:38:55 -0600

    init calling to medical api

--- api/v1/example/controller/assurance.ts
+++ api/v1/example/controller/assurance.ts
@@ -1,17 +1,51 @@
 import { z } from 'zod';
+import axios from 'axios';
 import { validateAssuranceSchema } from '../helpers/schemas';
 import { ApiError, ApiResponse, compareHashedPassword } from '../../../../helpers';
 
-const user = {
-    email: "example@example.com",
-    password: "$2a$12$xmU1v4tWlSpAKFUqDhQOV.5yhQWNLNsjohhVjvy9iay6qOH/GOU/y"
+
+
+
+const AUTH = {
+    username: "ingenieriaDigital",
+    password: "9YEL$m3Kcs?5"
 }
+
+
+let callMedicatelApi = async (data: z.infer<typeof validateAssuranceSchema>) => {
+    let result: any = await axios.post('https://api-dev.medicatel.red/cotizar/vida/seguros_plus', {
+        "edad": data.age,
+        "sumaAsegurada": data.insuredAmount,
+        "sexo": data.gender
+    }, {
+        auth: {
+            username: AUTH.username,
+            password: AUTH.password
+        }
+    }).catch((error: any) => {
+        if (error.response) {
+            console.log(error.response.status);
+        } else if (error.request) {
+            console.log("network error");
+        } else {
+            console.log(error);
+        }
+    });
+
+    console.log(result.statusCode)
+    if(result.statusCode === 200){
+        return result.data;
+    }
+    return null;
+}
+
 
 export async function AssuranceController(
     body: z.infer<typeof validateAssuranceSchema>
 ) {
+    let medicalAssurance = await callMedicatelApi(body);
 
-    console.log(body)
+    console.log(medicalAssurance)
 
     return new ApiResponse({
         statusCode: 200,
